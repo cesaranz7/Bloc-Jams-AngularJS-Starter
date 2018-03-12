987e9758f60d47e79de8e7689fca2bbaa4daaf73
cesaranz7 <cesaranz@hotmail.com>
2018-03-11 20:09:06 -0500

    services part 3 and descriptions complete

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,7 +1,16 @@
 (function() {
      function SongPlayer(Fixtures) {
+
+       /**
+       * @desc returns this object, making it's properties and methods public to the app
+       * @type {Object}
+       */
         var SongPlayer = {};
 
+        /**
+        * @desc pulls and sets current album data
+        * @type {Object}
+        */
         var currentAlbum = Fixtures.getAlbum();
 
         /**
@@ -31,6 +40,16 @@
         };
 
         /**
+        * @function stopSong
+        * @desc stops current currentBuzzObject
+        * @param {Object} song
+        */
+        var stopSong = function(song) {
+            currentBuzzObject.stop();
+            song.playing = null;
+        };
+
+        /**
         * @function playSong
         * @desc Plays selected currentBuzzObject and sets song.playing to true
         * @param {Object} song
@@ -40,12 +59,23 @@
             song.playing = true;
         };
 
+        /**
+        * @function getSongIndex
+        * @desc
+        * @param {Object} songs
+        * @returns {Number}
+        */
         var getSongIndex = function(song) {
             return currentAlbum.songs.indexOf(song);
         };
 
         SongPlayer.currentSong = null;
 
+        /**
+        * @function SongPlayer.play
+        * @desc plays a song
+        * @param {Object} song
+        */
         SongPlayer.play = function(song) {
             song = song || SongPlayer.currentSong;
             if (SongPlayer.currentSong !== song) {
@@ -59,23 +89,44 @@
 
         };
 
+        /**
+        * @function SongPlayer.pause
+        * @desc pauses song
+        * @param {Object} song
+        */
         SongPlayer.pause = function(song) {
             song = song || SongPlayer.currentSong;
             currentBuzzObject.pause();
             song.playing = false;
         };
 
+        /**
+        * @function SongPlayer.previous
+        * @desc moves back one song in the index to previous song
+        */
         SongPlayer.previous = function() {
             var currentSongIndex = getSongIndex(SongPlayer.currentSong);
             currentSongIndex--;
 
              if (currentSongIndex < 0) {
-                currentBuzzObject.stop();
-                SongPlayer.currentSong.playing = null;
+                stopSong();
              } else {
                 var song = currentAlbum.songs[currentSongIndex];
                 setSong(song);
                 playSong(song);
+            }
+        };
+
+        SongPlayer.next = function() {
+            var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+            currentSongIndex++;
+
+            if (currentSongIndex > currentSongIndex.length) {
+                stopSong();
+            } else {
+              var song = currentAlbum.songs[currentSongIndex];
+              setSong(song);
+              playSong(song);
             }
         };
 
