2fe8771d4d6d19b684f2129217ae234563aab65d
cesaranz7 <cesaranz@hotmail.com>
2018-03-26 21:52:48 -0500

    directives part 2 completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function() {
-     function SongPlayer(Fixtures) {
+     function SongPlayer( $rootScope, Fixtures) {
 
        /**
        * @desc returns this object, making it's properties and methods public to the app
@@ -36,6 +36,12 @@
                 preload: true
             });
 
+            currentBuzzObject.bind('timeupdate', function() {
+                $rootScope.$apply(function() {
+                    SongPlayer.currentTime = currentBuzzObject.getTime();
+                });
+            });
+
             SongPlayer.currentSong = song;
         };
 
@@ -70,6 +76,12 @@
         };
 
         SongPlayer.currentSong = null;
+
+        /**
+        * @desc Current playback time (in seconds) of currently playing song
+        * @type {Number}
+        */
+        SongPlayer.currentTime = null;
 
         /**
         * @function SongPlayer.play
@@ -130,11 +142,22 @@
             }
         };
 
+        /**
+        * @function setCurrentTime
+        * @desc Set current time (in seconds) of currently playing song
+        * @param {Number} time
+        */
+        SongPlayer.setCurrentTime = function (time) {
+            if (currentBuzzObject) {
+                currentBuzzObject.setTime(time);
+            }
+        };
+
 
     return SongPlayer;
 }
 
      angular
          .module('blocJams')
-         .factory('SongPlayer', SongPlayer);
+         .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
  })();
